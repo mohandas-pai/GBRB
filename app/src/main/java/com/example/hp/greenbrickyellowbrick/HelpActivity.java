3fc140ed7aa844bdb3a8618e017826bc47f9df3c
Mohandas Pai <amazing2196@gmail.com>
2018-09-30 21:27:41 +0530

    Added Help

--- app/src/main/java/com/example/hp/greenbrickyellowbrick/HelpActivity.java
+++ app/src/main/java/com/example/hp/greenbrickyellowbrick/HelpActivity.java
@@ -1,13 +1,28 @@
 package com.example.hp.greenbrickyellowbrick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
 
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        btnBack=(Button)findViewById(R.id.btnbck);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
